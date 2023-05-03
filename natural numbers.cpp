98ad8de7edaab0bff7ffbd2678cb69be7843eb9e
jaithra192210259 <130163958+jaithra192210259@users.noreply.github.com>
2023-05-03 14:48:22 +0530

    Add files via upload

---
+++ natural numbers.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main ()
+
+{
+	int n;
+	int i;
+
+	
+	printf("enter the value of n");
+	scanf("%d",&n);
+	
+	for(i=1;i<=n;i++)
+	{
+		printf("%d\n",i);
+	} 
+	
+	return 0;
+}
