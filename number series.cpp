faf90a8e228bf2e58917a2d580cd947225452cc7
jaithra192210259 <130163958+jaithra192210259@users.noreply.github.com>
2023-05-05 14:12:49 +0530

    Add files via upload

---
+++ number series.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n;
+	printf("enter the value of n (limit):");
+	scanf("%d",&n);
+	printf("\n");
+	for(i=1;i<=n;i++)
+	{
+		if(i==n)
+	     	printf("%d",i);
+		else
+		   printf("%d",i);
+		
+	}
+	getch();
+	return 0;
+}
